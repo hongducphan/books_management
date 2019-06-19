e2a49ecda7c3d63a372107a81b06fd94cb53b9fd
hongducphan <hongducphan912@gmail.com>
2019-06-20 00:46:11 +0700

    Update security config

--- src/main/java/com/ducph/springboot/config/SecurityConfig.java
+++ src/main/java/com/ducph/springboot/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll()
+        http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
